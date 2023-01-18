5ce4bf8b6a5764251b3fb4eacc3d9456abc98ecb
Laisa Pereira <laisasilva.pereira@yahoo.com>
2023-01-18 13:12:05 -0300

    metodos put post get by id funcionando

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+
 const port = 3000;
+
+app.use(bodyParser.json())
+
 
 app.get('/hello', (req, res) => {
   res.send('Hello World')
@@ -28,11 +34,35 @@ app.get('/mensagens', (req, res) => {
   res.send(mensagens)
 })
 
+// -- [GET] /mensagens/{id} -- retorna apenas uma unica mensagem pelo ID
+
 app.get('/mensagens/:id', (req,res) => {
   const id = (req.params.id) - 1
   const mensagem = mensagens[id]
 
   res.send(mensagem)
+})
+
+// -- [POST] /mensagens -- cria uma nova mensagem
+
+app.post('/mensagens', (req,res) =>{
+  const mensagem = req.body.mensagem;
+
+  mensagens.push(mensagem);
+
+  res.send(mensagem)
+})
+
+
+//- [PUT] /mensagens/{id} -- Atualiza uma mensagem pelo ID 
+
+app.put('/mensagens/:id', (req,res) => {
+  const id = (req.params.id ) - 1
+  const mensagem = req.body.mensagem
+
+  mensagens[id] = mensagem
+
+  res.send(`Mensagem atualizada com sucesso: '${mensagem}.'`)
 })
 
 
